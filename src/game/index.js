a1c5e9a6a468a4b9d086247ae5ba1022a7dd1e52
Colin Gourlay <colin@colin-gourlay.com>
2018-08-13 07:37:15 +1000

    Removed old Game state

--- src/game/index.js
+++ src/game/index.js
@@ -5,6 +5,5 @@ const game = new Phaser.Game(width, height, Phaser.AUTO, 'game');
 game.state.add('loading', require('./states/Loading'));
 game.state.add('menu', require('./states/Menu'));
 game.state.add('hill', require('./states/Hill'));
-game.state.add('game', require('./states/Game'));
 
 game.state.start('loading');
